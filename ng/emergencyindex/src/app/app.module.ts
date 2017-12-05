52112f616db730830e0aa60b84c92ae83b0bdb5a
edward sharp <edward@edwardsharp.net>
2017-12-04 21:51:24 -0500

    material-ize

--- ng/emergencyindex/src/app/app.module.ts
+++ ng/emergencyindex/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app-material.module';
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
