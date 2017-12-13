3d8d398b67ec5ac74d1b2e458365fa9b9872fde3
edward sharp <edward@edwardsharp.net>
2017-12-12 19:05:16 -0500

    - [x] project model validation

--- rails/emergencyindex/app/models/project.rb
+++ rails/emergencyindex/app/models/project.rb
@@ -1,2 +1,17 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class Project < ApplicationRecord
+
+  validates :name, length: { minimum: 3 }
+  validates :email, presence: true, email: true
+
+  validates :phone, :title, :first_date, :location, :dates, :artist_name, 
+    :collaborators, :home, :contact, :links, :description, presence: true
+
 end
