68bb6f65f73fbba43e8ad15625954c82f24997cf
edward sharp <edward@edwardsharp.net>
2018-01-22 05:12:25 -0500

    better (un)published search, tidy mat-select

--- rails/emergencyindex/app/assets/javascripts/admin.js
+++ rails/emergencyindex/app/assets/javascripts/admin.js
@@ -1,9 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-$(document).on('turbolinks:load', function() {
-	$('.mat-select').material_select();
-
-	$('#q_volume_year_in').material_select();
-
-});
