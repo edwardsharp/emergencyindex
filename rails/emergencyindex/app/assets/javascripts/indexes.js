b14f687a806cff129e87086e97aa18455d4d8c24
edward sharp <edward@edwardsharp.net>
2018-02-05 18:32:28 -0500

    mobile: fix accordion from opening off screen

--- rails/emergencyindex/app/assets/javascripts/indexes.js
+++ rails/emergencyindex/app/assets/javascripts/indexes.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).on('ready turbolinks:load', function() {
+  $('.indexes-collapsible').collapsible({
+    onOpen: function(el) { el[0].scrollIntoView(); }
+  });
+});
